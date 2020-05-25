75d9c96667415b4579d0dcd34bb782cb42304b27
Ale Marzano <alejandromarzano@encendercomunicacion.com>
2020-05-25 17:34:04 -0300

    Cierre previo a check de adidas

--- docs/js/events.js
+++ docs/js/events.js
@@ -1,29 +1,11 @@
 $(document).ready(function () {
 
+   $('')
 
-   $("a.nav-link[data-toggle='tab'").on('click', function (e) {
-      id = ($(this).attr('href'));
-      content = $(this).parents('ul').next('.tab-content');
-      tabActive = content.find(id)
-
-      loader = tabActive.children('.loader');
-      // flow = tabActive.children('div.flowics-viz');
-
-      function showPage() {
-         loader.hide();
-         // flow.show();
-      }
-      setTimeout(showPage, 3000);
-
-   })
 
    /* Smooth Scroll */
-   // Select all links with hashes
-   $('a.anchor[href*="#"]')
-      // Remove links that don't actually link to anything
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function (event) {
+   //Select all links with hashes
+   $('a.anchor[href*="#"]').click(function (event) {
          // On-page links
          if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
@@ -31,6 +13,7 @@ $(document).ready(function () {
          ) {
             // Figure out element to scroll to
             var target = $(this.hash);
+            
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             // Does a scroll target exist?
             if (target.length) {
@@ -43,4 +26,7 @@ $(document).ready(function () {
             }
          }
       });
+
+
+
 });
